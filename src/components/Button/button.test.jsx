a573124c125202ca76b168e594475ab650a8c764
webInRoad <1040233545@qq.com>
2021-03-07 22:21:18 +0800

    button 用例提交

--- src/components/Button/button.test.jsx
+++ src/components/Button/button.test.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './button'
 
-test('test button', () => {
-	const wrapper = render(<Button>Nice</Button>)
-	const element = wrapper.queryByText('Nice')
-	expect(element).toBeTruthy()
+// test('test button', () => {
+// 	const wrapper = render(<Button>Nice</Button>)
+// 	const element = wrapper.queryByText('Nice')
+// 	expect(element).toBeTruthy()
+// })
+describe('test button component', () => {
+	it('should render the correct default button', () => {
+		const wrapper = render(<Button>Nice</Button>)
+		const element = wrapper.queryByText('Nice')
+		expect(element).toBeInTheDocument()
+		expect(element.tagName).toBe('BUTTON')
+		expect(element).toHaveClass('btn btn-default')
+	})
 })
