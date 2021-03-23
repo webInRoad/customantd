b47699f328740629251e3a6ffd21f647332198b5
webInRoad <1040233545@qq.com>
2021-03-23 12:01:36 +0800

    Input story提交

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
 import { Input, InputProps } from './input'
 
@@ -13,4 +13,44 @@ const Template: Story<InputProps> = (args) => (
 export const defaultInput = Template.bind({})
 defaultInput.args = {
 	placeholder: 'test'
+}
+
+export const sizeInput = Template.bind({})
+sizeInput.args = {
+	placeholder: 'test',
+	size: 'lg'
+}
+
+export const disabledInput = Template.bind({})
+disabledInput.args = {
+	placeholder: 'disabled input',
+	disabled: true
+}
+
+export const iconInput = Template.bind({})
+iconInput.args = {
+	placeholder: 'icon input',
+	icon: 'search'
+}
+export const prependInput = Template.bind({})
+prependInput.args = {
+	placeholder: 'prepend input',
+	defaultValue: 'prepend value',
+	prepend: 'https://'
+}
+export const appendInput = Template.bind({})
+appendInput.args = {
+	placeholder: 'append input',
+	defaultValue: 'google',
+	append: '.com'
+}
+export const ControlInput = () => {
+	const [value, setVal] = useState('')
+	return (
+		<Input
+			value={value}
+			defaultValue={value}
+			onChange={(e) => setVal(e.target.value)}
+		></Input>
+	)
 }
--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -6,12 +6,19 @@ import Icon from '../Icon/icon'
 type inputSize = 'lg' | 'sm'
 export interface InputProps
 	extends Omit<InputHTMLAttributes<HTMLElement>, 'size'> {
+	/**设置input 大小，支持lg或者是sm */
 	size?: inputSize
-	disbaled?: boolean
+	/**是否禁用 Input */
+	disabled?: boolean
+	/**添加图标，在右侧悬浮添加一个图标，用于提示 */
 	icon?: IconProp
-	prepend?: string | React.ReactNode
-	append?: string | React.ReactNode
+	/**添加前缀 用于配置一些固定组合 */
+	prepend?: string | React.ReactElement
+	/**添加后缀 用于配置一些固定组合 */
+	append?: string | React.ReactElement
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
+/** input输入框 */
 export const Input: React.FC<InputProps> = (props) => {
 	const { disabled, size, icon, prepend, append, style, ...restProps } = props
 	const cnames = classNames('input-wrapper', {
@@ -21,6 +28,16 @@ export const Input: React.FC<InputProps> = (props) => {
 		'input-group-prepend': prepend,
 		'input-group-append': append
 	})
+	const fixControlledValue = (value: any) => {
+		if (typeof value == 'undefined' || value === 'null') {
+			return ''
+		}
+		return value
+	}
+	if ('value' in props) {
+		delete props.defaultValue //value与defaultValue只能存在一个，不能既是受控，又是非受控组件
+		restProps.value = fixControlledValue(props.value) //当value没有设置初始值时给予""
+	}
 	return (
 		<div className={cnames} style={style}>
 			{prepend && <div className="input-group-prepend">{prepend}</div>}
