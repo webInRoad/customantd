56218a6a2d4344507ca52c3acf0b8dd3dbc1763d
webInRoad <1040233545@qq.com>
2021-03-24 16:00:19 +0800

    autoComplete自定义下拉渲染格式

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,25 +1,50 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { AutoComplete, AutoCompleteProps } from './autoComplete'
+import { AutoComplete, AutoCompleteProps, DataSourceType } from './autoComplete'
 export default {
 	title: 'autoComplete',
 	component: AutoComplete
 } as Meta
+interface ballDataProps {
+	name: string
+	num: number
+}
+// const ballData = [
+// 	'volleyball',
+// 	'table tennis',
+// 	'basketball',
+// 	'soccer',
+// 	'badminton'
+// ]
 const ballData = [
-	'volleyball',
-	'table tennis',
-	'basketball',
-	'soccer',
-	'badminton'
+	{ name: 'volleyball', num: 23 },
+	{ name: 'table tennis', num: 22 },
+	{ name: 'basketball', num: 12 },
+	{ name: 'soccer', num: 32 },
+	{ name: 'badminton', num: 17 }
 ]
 const Template: Story<AutoCompleteProps> = (args) => (
 	<AutoComplete {...args}></AutoComplete>
 )
-const filterData = (item: string) => {
-	return ballData.filter((data) => data.includes(item))
+const renderOption = (item: DataSourceType) => {
+	const data = item as DataSourceType<ballDataProps> //断言
+	return (
+		<>
+			<h2>Name:{data.name}</h2>
+			<p>人数:{data.num}</p>
+		</>
+	)
+}
+const filterData = (item: DataSourceType) => {
+	return ballData.filter((data) => data.name.includes(item.name))
+}
+const onSelect = (item: DataSourceType) => {
+	alert(item.name)
 }
 export const defaultAutoComplete = Template.bind({})
 
 defaultAutoComplete.args = {
-	onChange: filterData
+	onChange: filterData,
+	renderOption,
+	onSelect
 }
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { Input, InputProps } from '../Input/input'
+interface DataSourceObject {
+	name: string
+}
 
+export type DataSourceType<T = {}> = T & DataSourceObject
 export interface AutoCompleteProps
 	extends Omit<InputProps, 'onChange' | 'onSelect'> {
-	onChange?: (item: string) => string[]
-	onSelect?: (item: string) => void
+	onChange?: (item: DataSourceType) => DataSourceType[]
+	onSelect?: (item: DataSourceType) => void
+	renderOption?: (item: DataSourceType) => ReactElement
 }
 export const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
-	const { onChange, onSelect, value, ...restProps } = props
+	const { onChange, onSelect, renderOption, value, ...restProps } = props
 	const [inputValue, setInputValue] = useState(value)
-	const [dropDownData, setDropDownData] = useState<string[]>()
+	const [dropDownData, setDropDownData] = useState<DataSourceType[]>()
 	const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.value.trim()
 		setInputValue(value)
 		if (value) {
-			const data = onChange && onChange(value)
+			const data = onChange && onChange({ name: value })
 			setDropDownData(data)
 		} else {
 			setDropDownData([])
 		}
+	}
+	const renderTemplate = (item: DataSourceType) => {
+		return renderOption ? renderOption(item) : item.name
+	}
+	const handleOnSelect = (item: DataSourceType) => {
+		setInputValue(item.name)
+		setDropDownData([])
+		onSelect && onSelect(item)
 	}
 	const generateDropDown = () => {
 		return (
 			dropDownData && (
 				<ul>
 					{dropDownData.map((item, index) => {
-						return <li key={index}>{item}</li>
+						return (
+							<li key={index} onClick={() => handleOnSelect(item)}>
+								{renderTemplate(item)}
+							</li>
+						)
 					})}
 				</ul>
 			)
@@ -33,7 +50,7 @@ export const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
 	}
 	return (
 		<div>
-			<Input value={inputValue} onChange={handleOnChange} />
+			<Input value={inputValue} onChange={handleOnChange} {...restProps} />
 			{generateDropDown()}
 		</div>
 	)
